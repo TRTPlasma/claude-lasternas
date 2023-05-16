7f5e8f8b825a0a0d78f011c0dd081fba908e7921
Wems-Matteo <78219412+Wems-Matteo@users.noreply.github.com>
2023-05-16 21:16:26 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
 'use strict';
 
 import { useRouter } from 'next/router';
+import { Text } from '@chakra-ui/react';
 import { Fragment, useCallback, useEffect, useState } from 'react';
 import useData from '../components/hooks/useData';
 import Heading from '../components/Heading';
 import Grid from '../components/Grid';
+import Link from 'next/link';
 
 export default function Index() {
   const { push } = useRouter();
@@ -25,6 +27,10 @@ export default function Index() {
         Voici un aperçu de mes peintures:
       </Heading>
       <Grid data={data} message='Voir plus' callback={callback} />
+      <Text fontSize={20} margin='20px'>
+        Il y a la possibilité de réaliser des tableaux sur des thèmes personnalisés !
+        Vous pouvez me <Link href='/contact'><strong>contacter</strong></Link>
+      </Text>
     </Fragment>
   );
 }
